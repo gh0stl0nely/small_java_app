1fa49ffd7637b8f53d86d35b5dfde6ada3ac5baf
gh0stl0nelyGit <khoinguyen19971997@gmail.com>
2021-04-29 22:28:25 -0400

    Initialized base class

---
+++ src/Hippodrome.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Hippodrome {
+    private ArrayList<Horse> horses = new ArrayList();
+    public static void main(String[] args) {
+        
+    }
+
+    public ArrayList<Horse> getHorses(){
+        return horses;
+    }
+}
---
+++ src/Horse.java
@@ -0,0 +1,5 @@
+public class Horse {
+    public static void main(String[] args) {
+        
+    }
+}
