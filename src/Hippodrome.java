8a3ce89912b0ef42895a756023ba27c4a783d946
gh0stl0nelyGit <khoinguyen19971997@gmail.com>
2021-04-29 22:50:02 -0400

    Initialized game

--- src/Hippodrome.java
+++ src/Hippodrome.java
@@ -1,12 +1,21 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Hippodrome {
-    private ArrayList<Horse> horses = new ArrayList();
+    private static Hippodrome game;
+    private ArrayList<Horse> horses = new ArrayList<Horse>();
     public static void main(String[] args) {
-        
+        ArrayList<Horse> horseList = new ArrayList<Horse>();
+        game = new Hippodrome(horseList);
+        ArrayList<Horse> horses = game.getHorses();
+        String name = null;
+        for(int i = 0; i <= 2; i++){
+            name = i == 0 ? "Andy" : (i == 1 ? "Bobby" : "Candy");
+            horses.add(new Horse(name, 3.0,0));
+        }
     }
 
-    public Hippodrome(List<Horse> horses){
+    public Hippodrome(ArrayList<Horse> horses){
         this.horses = horses;
     }
 
